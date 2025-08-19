a45ab1fe581ab6242a998eaf5f134152adde5a8b
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-19 22:11:21 +0530

    Update 0258-add-digits.java

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -14,7 +14,12 @@ class Solution {
         }
         return sum;
         */
-        //Using Digital Root concept
+        /*Using Digital Root concept
+Instead of looping to add digits over and over, you can use a simple math trick called the digital root.
+Here’s the idea:
+In base 10, every place value (10, 100, 1000…) leaves a remainder of 1 when divided by 9. So taking a number 
+modulo 9 gives the same result as, Repeating that sum until one digit remains.
+*/
         if(num == 0)
         return 0;
         else if(num % 9 == 0)
