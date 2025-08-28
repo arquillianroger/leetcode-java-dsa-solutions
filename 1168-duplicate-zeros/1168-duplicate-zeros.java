249e65407a6cf657c3bbb1031e08373ec4df82d5
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-28 16:19:12 +0530

    Update 1168-duplicate-zeros.java

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,5 +1,23 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
+        /*Using O(n) Space
+        int i = 0; 
+        int j = 0;
+        int[] temp = new int[arr.length];
+        while(j < arr.length){
+            if(arr[i] == 0 && j < arr.length - 1){
+                temp[j++] = arr[i++];
+                temp[j++] = 0;
+            }
+            else{
+                temp[j++] = arr[i++];
+            }
+        }
+        for(i = 0; i < arr.length; i++){
+            arr[i] = temp[i];
+        }
+        */
+        // Using O(n) Space
         int zerosToDuplicate = 0;
         int n = arr.length - 1;
         for (int i = 0; i <= n - zerosToDuplicate; i++) {
