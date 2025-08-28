37716f17a7544846f5d0f5c0e30b9d4f7d30e856
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-28 16:13:24 +0530

    Time: 1 ms (99.03%), Space: 44.7 MB (79.58%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,19 +1,27 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int i = 0; 
-        int j = 0;
-        int[] temp = new int[arr.length];
-        while(j < arr.length){
-            if(arr[i] == 0 && j < arr.length - 1){
-                temp[j++] = arr[i++];
-                temp[j++] = 0;
-            }
-            else{
-                temp[j++] = arr[i++];
+        int zerosToDuplicate = 0;
+        int n = arr.length - 1;
+        for (int i = 0; i <= n - zerosToDuplicate; i++) {
+            if (arr[i] == 0) {
+                // Edge case: If the current zero would be the last element we can process
+                if (i == n - zerosToDuplicate) {
+                    arr[n] = 0;
+                    n -= 1;
+                    // Special handling for the last zero
+                    break;
+                }
+                zerosToDuplicate++;
             }
         }
-        for(i = 0; i < arr.length; i++){
-            arr[i] = temp[i];
+        for(int i = n - zerosToDuplicate; i >= 0; i--) {
+            if (arr[i] == 0 && zerosToDuplicate > 0) {
+                arr[i + zerosToDuplicate] = arr[i];
+                zerosToDuplicate -= 1;
+                arr[i + zerosToDuplicate] = arr[i];
+            } else {
+                arr[i + zerosToDuplicate] = arr[i];
+            }
         }
     }
 }
