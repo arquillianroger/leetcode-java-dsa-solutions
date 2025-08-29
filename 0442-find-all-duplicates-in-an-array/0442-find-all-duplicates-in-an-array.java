44783141ca05818b15c67d489437a8fa66fed86d
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-29 18:24:47 +0530

    Time: 6 ms (83.31%), Space: 57.2 MB (49.53%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -9,11 +9,29 @@ class Solution {
             }
         }
         */
-        //Sorting Based Approach : O(nlogn)
+        /*Sorting Based Approach : O(nlogn)
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 1; i++){
             if(nums[i] == nums[i + 1] && !duplicates.contains(nums[i]))
             duplicates.add(nums[i]);
+        }
+        */
+        /*Optimized Approach : O(n) --Index Marking Trick
+        So when you encounter a number x:
+        1. Jump to index x-1.
+        2. If it’s still positive → mark it negative → “first visit”.
+        3. If it’s already negative → “second visit” → that’s a duplicate.
+        */
+        for(int i = 0; i < nums.length; i++){
+            int x = Math.abs(nums[i]);
+            if(nums[x - 1] > 0)
+            nums[x - 1] = - nums[x - 1];
+            else
+            duplicates.add(x);
+        }
+        //Restoring the array back to its original state
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Math.abs(nums[i]);
         }
         return duplicates;
     }
