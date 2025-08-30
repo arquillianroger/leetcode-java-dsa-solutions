58fdd7e5756734a3a5f77845831a99ba6690a601
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-30 17:02:07 +0530

    Time: 0 ms (100%), Space: 45.4 MB (7.49%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        //Brute Force Approach : O(n^2) --Check Every Possible Subarray
+        /*Brute Force Approach : O(n^2) --Check Every Possible Subarray
         int productMax = nums[0];
         for (int i = 0; i < nums.length; i++) {
             int product = nums[i];
@@ -10,7 +10,31 @@ class Solution {
                 productMax = Math.max(product, productMax);
             }
         }
-        //
+        */
+        /*Prefix and Suffix scan : O(n)
+        1. Multiplying across an even number of negatives gives a positive (good).
+        2. Multiplying across an odd number of negatives gives a negative (bad).
+        3. Depending on where the “extra negative” lies (left or right), the max product might come from trimming the prefix or trimming the suffix.
+        So, by scanning both left→right and right→left, we guarantee that we’ve considered both possibilities.
+        */
+        int currProduct = 1;
+        int productMax = nums[0];
+        //Scan 1 : Left to Right Scan
+        for(int i = 0; i < nums.length; i++){
+          currProduct *= nums[i];
+          productMax = Math.max(currProduct, productMax);
+          if(currProduct == 0)
+            currProduct = 1;
+        }
+
+        currProduct = 1;
+        //Scan 2 : Right to Left Scan
+        for(int i = nums.length - 1; i >= 0; i--){
+          currProduct *= nums[i];
+          productMax = Math.max(currProduct, productMax);
+          if(currProduct == 0)
+            currProduct = 1;
+        }
         return productMax;
     }
 }
