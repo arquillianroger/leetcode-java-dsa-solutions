00cd30b005bca72ccdd94ed42b128b6710c36284
Manikya Sinha <153370298+arquillianroger@users.noreply.github.com>
2025-08-30 23:10:24 +0530

    Update 0229-majority-element-ii.java

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,30 +1,23 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        /*Brute Force Approach : O()
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = nums.length;
+        /*Brute Force Approach : O(n^2)
         Let’s say a, b, and c all appear more than ⌊n/3⌋ times.
-        That means:
-        freq(a) > n/3, freq(b) > n/3, freq(c) > n/3
+        That means: freq(a) > n/3, freq(b) > n/3, freq(c) > n/3
         If you add them up: freq(a) + freq(b) + freq(c) > n
         But the array only has n elements total!
         Therefore: At most 2 elements can have frequency > ⌊n/3⌋.
-        */
-        ArrayList<Integer> list = new ArrayList<>();
-        int n = nums.length;
-        int loopCounter = 0;
         for(int i = 0; i < n; i++){
             int count = 0;
-            if(loopCounter <= 2) // As, At most 2 elements can have frequency > ⌊n/3⌋.
-            {
-                for(int j = 0; j < n; j++){
-                    if(nums[i] == nums[j])
+            for(int j = 0; j < n; j++){
+                if(nums[i] == nums[j])
                     count ++;
-                }
-                if(count > Math.floor(n / 3) && !list.contains(nums[i])){
-                    list.add(nums[i]);
-                    loopCounter ++;
-                }
             }
+            if(count > n / 3 && !list.contains(nums[i]))// n/3 gives integer result so Math.floor() is not required
+                list.add(nums[i]);
         }
+        */
         return list;
     }
 }
